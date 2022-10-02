c044358aa901eed2bbb9c3a168c7892e9c322fa6
PCRVLX <imhobff@gmail.com>
2022-10-02 16:46:34 +0300

    create header/main_banner/recipe/order/magna/advantages/eat/reviews+swiper add

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
-    direction: 'vertical',
     loop: true,
-
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
     },
-
-    // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
     },
 });
